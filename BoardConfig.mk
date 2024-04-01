f34e865869122ab03f29391d4bb3bd2986a5717e
NITHESH <112474206+nitheshd30@users.noreply.github.com>
2024-04-01 10:27:13 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ BOARD_USES_MTK_HARDWARE := true
 
 ## TWRP-Specific configuration
 TW_THEME := portrait_hdpi
-TW_DEVICE_VERSION := 3.6.2_11-Jprimero15
+TW_DEVICE_VERSION := Jprimero15
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_USES_MKE2FS := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
